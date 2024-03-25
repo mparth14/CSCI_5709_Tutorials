b352de54a8f584caf64fc805701076455e94534e
Parth Modi <pmodi1080@gmail.com>
2024-03-25 10:09:21 -0300

    Update App.js

--- Tutorial 7/Tutorial7/App.js
+++ Tutorial 7/Tutorial7/App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Connection URI
 const uri =
